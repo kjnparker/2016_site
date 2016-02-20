2f3edecdd0c3a0307e43596ba2a6a526ae7fb082
ackermcc <ackermcc@gmail.com>
2016-02-20 10:15:30 -0800

    completed filter functionality

--- scripts/projectFilter.js
+++ scripts/projectFilter.js
@@ -1,33 +1,53 @@
 $(document).ready(function(){
-updateVisibleProjects();
 
-function updateVisibleProjects(classifications) {
+//On filter bar option click, grab ID nd update visible projects
+$("#filter-bar .filterTag").on('click', function(){
+  var _ = $(this),
+      newClassification = _.attr('id'),
+      currentClassfication = $(".selected-filter").attr('id');
+
+  if(newClassification !== currentClassfication){
+    //Update filter selected state
+    $("#filter-bar .filterTag").removeClass("selected-filter")
+    _.addClass("selected-filter");
+    updateVisibleProjects(newClassification)
+  }
+});
+
+function updateVisibleProjects(classification) {
   //Set up variables
-  var projects = $("#home-projects avisible").is("visible"),
+  var visibleProjects = $("#home-projects a:visible"),
+      allProjects = $("#home-projects a"),
       environmental = $(".environmental"),
-      identity = $('identity'),
-      iconography = $('iconography'),
-      photography = $('photography');
+      identity = $('.identity'),
+      iconography = $('.iconography'),
+      photography = $('.photography');
 
   //Fade out all projects
-  projects.fadeOut(300);
+  visibleProjects.fadeOut(500, function(){
+    switch (classification) {
+      case "environmental":
+        //Show environmental projects
+        environmental.fadeIn(500);
+        break;
+      case "identity":
+        //Show identity projects
+        identity.fadeIn(500);
+        break;
+      case "iconography":
+        //Show iconography projects
+        iconography.fadeIn(500);
+        break;
+      case "photography":
+        //Show photography projects
+        photography.fadeIn(500);
+        break;
+      default:
+        //Show all projects
+        allProjects.fadeIn(500);
+    }
+  });
 
-  switch (classifications) {
-    case "environmental":
-      //Show environmental projects
-      break;
-    case "identity":
-      //Show identity projects
-      break;
-    case "iconography":
-      //Show iconography projects
-      break;
-    case "photography":
-      //Show photography projects
-      break;
-    default:
-      //Show all projects
-  }
 }
 
 });
