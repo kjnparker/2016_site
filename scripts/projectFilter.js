cdb358399e8426a2480bc362e9ac38f679213fa5
ackermcc <ackermcc@gmail.com>
2016-02-20 09:54:28 -0800

    set up initial filter structure

--- scripts/projectFilter.js
+++ scripts/projectFilter.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
+updateVisibleProjects();
+
+function updateVisibleProjects(classifications) {
+  //Set up variables
+  var projects = $("#home-projects avisible").is("visible"),
+      environmental = $(".environmental"),
+      identity = $('identity'),
+      iconography = $('iconography'),
+      photography = $('photography');
+
+  //Fade out all projects
+  projects.fadeOut(300);
+
+  switch (classifications) {
+    case "environmental":
+      //Show environmental projects
+      break;
+    case "identity":
+      //Show identity projects
+      break;
+    case "iconography":
+      //Show iconography projects
+      break;
+    case "photography":
+      //Show photography projects
+      break;
+    default:
+      //Show all projects
+  }
+}
 
 });
